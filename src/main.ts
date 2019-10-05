84830d657f2c298cb1ed9aa3bd1141bc8d7595b1
Tom Irimescu <tom@irimescu.com>
2019-10-05 19:27:38 -0400

    port config update

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import * as helmet from 'helmet';
+import { ConfigService } from './services/config/config.service';
 
 async function bootstrap() {
   // create an app instance with CORS enabled
@@ -13,6 +14,6 @@ async function bootstrap() {
   // define a global validation pipe
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
 
-  await app.listen(process.env.PORT);
+  await app.listen(new ConfigService().get('PORT'));
 }
 bootstrap();
